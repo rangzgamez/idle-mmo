e35e49bb1c4b04f6bb980d21148c9cc61a8c77b6
Daniel Rangel <rangzgamez@gmail.com>
2025-03-20 14:46:26 -0400

    remove dist

--- backend/dist/characters/dto/create-character.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { CharacterClass } from '../entities/character.entity';
-export declare class CreateCharacterDto {
-    name: string;
-    class: CharacterClass;
-    userId: string;
-    skills?: any[];
-}
